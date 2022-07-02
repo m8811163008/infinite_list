dc6d1b4f3ce9a678509f0eee065eba21d7159267
localadmin <m8811163008@gmail.com>
2022-07-02 13:45:48 +0430

    [test] add post status failure when call to server is unsuccessful

--- lib/posts/domain/business_logic_bloc/post_bloc.dart
+++ lib/posts/domain/business_logic_bloc/post_bloc.dart
@@ -1,16 +1,30 @@
 import 'package:bloc/bloc.dart';
+import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:equatable/equatable.dart';
 import 'package:infinit_list/posts/domain/models/post.dart';
 import 'package:infinit_list/posts/domain/repository.dart';
+import 'package:stream_transform/stream_transform.dart';
 
 part 'post_event.dart';
 part 'post_state.dart';
+
+const _postLimit = 20;
+const throttleDuration = Duration(milliseconds: 100);
+
+EventTransformer<E> throttleDroppable<E>(Duration duration) {
+  return (events, mapper) {
+    return droppable<E>().call(events.throttle(duration), mapper);
+  };
+}
 
 class PostBloc extends Bloc<PostEvent, PostState> {
   final PostRepository postRepository;
 
   PostBloc(this.postRepository) : super(const PostState()) {
-    on<PostFetched>(_onPostFetched);
+    on<PostFetched>(
+      _onPostFetched,
+      transformer: throttleDroppable(throttleDuration),
+    );
   }
 
   void _onPostFetched(PostFetched event, Emitter<PostState> emit) async {
--- test/posts/domain/business_logic_bloc/post_bloc_test.dart
+++ test/posts/domain/business_logic_bloc/post_bloc_test.dart
@@ -1,5 +1,6 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:infinit_list/core/error/exception.dart';
 import 'package:infinit_list/posts/domain/business_logic_bloc/post_bloc.dart';
 import 'package:infinit_list/posts/domain/models/post.dart';
 import 'package:infinit_list/posts/domain/repository.dart';
@@ -47,6 +48,70 @@ void main() {
                     postStatus: PostStatus.success,
                     posts: mockPosts,
                     hasReachedMax: false),
+              ],
+          verify: (_) {
+            verify(() => mockPostRepository.fetchPosts()).called(1);
+          });
+
+      blocTest<PostBloc, PostState>(
+          'drops new events when processing current event',
+          setUp: () {
+            when(() => mockPostRepository.fetchPosts())
+                .thenAnswer((_) async => mockPosts);
+          },
+          build: () => sut,
+          act: (bloc) {
+            bloc
+              ..add(PostFetched())
+              ..add(PostFetched());
+          },
+          expect: () => <PostState>[
+                const PostState(
+                  postStatus: PostStatus.success,
+                  posts: mockPosts,
+                  hasReachedMax: false,
+                )
+              ],
+          verify: (_) {
+            verify(() => mockPostRepository.fetchPosts()).called(1);
+          });
+
+      blocTest<PostBloc, PostState>('throttles events',
+          setUp: () {
+            when(() => mockPostRepository.fetchPosts())
+                .thenAnswer((_) async => mockPosts);
+          },
+          build: () => sut,
+          act: (bloc) async {
+            bloc.add(PostFetched());
+            await Future<void>.delayed(Duration.zero);
+            bloc.add(PostFetched());
+          },
+          expect: () => <PostState>[
+                const PostState(
+                  postStatus: PostStatus.success,
+                  posts: mockPosts,
+                  hasReachedMax: false,
+                )
+              ],
+          verify: (_) {
+            verify(() => mockPostRepository.fetchPosts()).called(1);
+          });
+
+      blocTest<PostBloc, PostState>(
+          'emits failure status when http fetches posts and throw exception',
+          build: () => sut,
+          setUp: () {
+            when(() => mockPostRepository.fetchPosts())
+                .thenThrow(ServerException());
+          },
+          act: (bloc) {
+            bloc.add(PostFetched());
+          },
+          expect: () => <PostState>[
+                const PostState(
+                  postStatus: PostStatus.failure,
+                )
               ],
           verify: (_) {
             verify(() => mockPostRepository.fetchPosts()).called(1);
