2cdf14b57dc6c7e2faff97e0d1e422984e90b18c
localadmin <m8811163008@gmail.com>
2022-07-02 11:47:28 +0430

    [passed]add post bloc state with enum and copy with pattern to preserve posts

--- lib/posts/domain/business_logic_bloc/post_bloc.dart
+++ lib/posts/domain/business_logic_bloc/post_bloc.dart
@@ -1,13 +1,12 @@
-import 'dart:async';
-
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:infinit_list/posts/domain/models/post.dart';
 
 part 'post_event.dart';
 part 'post_state.dart';
 
 class PostBloc extends Bloc<PostEvent, PostState> {
-  PostBloc() : super(PostInitial()) {
+  PostBloc() : super(const PostState()) {
     on<PostEvent>((event, emit) {
       // TODO: implement event handler
     });
--- lib/posts/domain/business_logic_bloc/post_state.dart
+++ lib/posts/domain/business_logic_bloc/post_state.dart
@@ -1,10 +1,34 @@
 part of 'post_bloc.dart';
 
-abstract class PostState extends Equatable {
-  const PostState();
-}
+enum PostStatus { initial, success, failure }
 
-class PostInitial extends PostState {
+class PostState extends Equatable {
+  final PostStatus postStatus;
+  final bool hasReachedMax;
+  final List<Post> posts;
+
+  const PostState(
+      {this.postStatus = PostStatus.initial,
+      this.hasReachedMax = false,
+      this.posts = const <Post>[]});
+
+  PostState copyWith({
+    PostStatus? postStatus,
+    bool? hasReachedMax,
+    List<Post>? posts,
+  }) {
+    return PostState(
+      postStatus: postStatus ?? this.postStatus,
+      hasReachedMax: hasReachedMax ?? this.hasReachedMax,
+      posts: posts ?? this.posts,
+    );
+  }
+
   @override
-  List<Object> get props => [];
+  String toString() {
+    return 'PostState{postStatus: $postStatus, hasReachedMax: $hasReachedMax, posts: $posts}';
+  }
+
+  @override
+  List<Object> get props => [postStatus, hasReachedMax, posts];
 }
